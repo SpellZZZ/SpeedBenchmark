4dbe3b21e47b7dbbbf5c0e721f278cc768344b18
SpellZZZ <43863065+SpellZZZ@users.noreply.github.com>
2022-05-28 13:30:32 +0200

    Update AppBody.java

--- src/AppBody.java
+++ src/AppBody.java
@@ -2,9 +2,14 @@ import org.jfree.chart.ChartPanel;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.Objects;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import javax.swing.text.*;
+
+import static java.awt.Color.WHITE;
 
 
 class AppBody extends Frame implements ActionListener, KeyListener {
@@ -16,7 +21,7 @@ private
     int taps = 0;
     double bpm = 0;
 
-
+    Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
 
     // timer manipulation
     TimerBuild timerInit;
@@ -53,11 +58,11 @@ private
     JSpinner tTime = new JSpinner(model1);
 
     // JLabel, (top panel)
-    JLabel lKey1 = new JLabel("Key1");
-    JLabel lKey2 = new JLabel("Key2");
-    JLabel lTime = new JLabel("Time");
-    JLabel lPodz = new JLabel("Divisor");
-    JLabel lSec = new JLabel("seconds");
+    JLabel lKey1 = new JLabel(" Key1 ");
+    JLabel lKey2 = new JLabel(" Key2 ");
+    JLabel lTime = new JLabel(" Time ");
+    JLabel lPodz = new JLabel(" Divisor ");
+    JLabel lSec = new JLabel(" seconds ");
     JLabel result = new JLabel(
             "<html>" +
                     "Taps: " + taps +
@@ -80,12 +85,12 @@ private
 
         graph = Chart.initUI();
         window =  new JFrame();
+        graph.setBackground(new Color(38, 35, 53));
 
         setTimer();
         setMenu();
         setResult();
 
-        panelResult.setBackground(Color.LIGHT_GRAY);
 
         window.add(panelMain, BorderLayout.NORTH);
         window.add(panelResult, BorderLayout.CENTER);
@@ -95,10 +100,14 @@ private
         rRestart.addActionListener(this);
         tPodz.addActionListener(this);
 
+        window.setIconImage(icon);
+        window.setResizable(false);
         window.pack();
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+
 
     public void setTimer(){
         timerInit = new TimerBuild(timeField);
@@ -112,24 +121,71 @@ private
         tKey1.setDocument(new LimitJTextField(1));
         tKey2.setDocument(new LimitJTextField(1));
         tPodz.setSelectedIndex(3);
-
-
-
         tKey1.setText("b");
         tKey2.setText("n");
-        panelMain.add(lKey1);
-        panelMain.add(tKey1);
-        panelMain.add(lKey2);
-        panelMain.add(tKey2);
-        panelMain.add(lPodz);
-        panelMain.add(tPodz);
-        panelMain.add(lTime);
-        panelMain.add(tTime);
+
+        lKey1.setForeground(new Color(	111, 138, 233));
+        lKey2.setForeground(new Color(	111, 138, 233));
+        lPodz.setForeground(new Color(	111, 138, 233));
+        lTime.setForeground(new Color(	111, 138, 233));
+
+        tKey1.setForeground(new Color(	222, 68, 80));
+        tKey2.setForeground(new Color(	222, 68, 80));
+        tPodz.setForeground(new Color(	222, 68, 80));
+
+        tKey1.setBackground(new Color(36, 24, 38));
+        tKey2.setBackground(new Color(36, 24, 38));
+        tPodz.setBackground(new Color(36, 24, 38));
+
+        JComponent editor = tTime.getEditor();
+        int n = editor.getComponentCount();
+        for (int i=0; i<n; i++)
+        {
+            Component c = editor.getComponent(i);
+            if (c instanceof JTextField)
+            {
+                c.setForeground(new Color(	222, 68, 80));
+                c.setBackground(new Color(36, 24, 38));
+
+            }
+        }
+
+
+        tKey1.setBorder(BorderFactory.createEmptyBorder());
+        tKey2.setBorder(BorderFactory.createEmptyBorder());
+        tTime.setBorder(BorderFactory.createEmptyBorder());
+
+
+        for (int i = 0; i < tPodz.getComponentCount(); i++)
+        {
+            if (tPodz.getComponent(i) instanceof JComponent) {
+                ((JComponent) tPodz.getComponent(i)).setBorder(BorderFactory.createEmptyBorder());
+            }
+
+
+            if (tPodz.getComponent(i) instanceof AbstractButton) {
+                ((AbstractButton) tPodz.getComponent(i)).setBorderPainted(false);
+            }
+        }
+
+
+
+
+        panelMain.add(lKey1).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(tKey1).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(lKey2).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(tKey2).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(lPodz).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(tPodz).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(lTime).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelMain.add(tTime).setFont(new Font("Tahoma", Font.BOLD, 20));
         panelMain.add(bStart);
         panelMain.add(rRestart);
-        panelMain.setBackground(Color.LIGHT_GRAY);
-        result.setBorder(BorderFactory.createEmptyBorder(5, 0, 10, 0));
-        //panelMain.setFont(new Font("Comic Sans",Font.BOLD,25));
+
+
+        panelMain.setBorder(BorderFactory.createEmptyBorder(5, 0, 10, 0));
+        panelMain.setBackground(new Color(38, 35, 53));
+
     }
     public void setResult(){
 
@@ -137,19 +193,29 @@ private
         result.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
 
         timeField.setHorizontalAlignment(JLabel.CENTER);
+        timeField.setForeground(new Color(	222, 68, 80));
+        timeField.setBackground(new Color(38, 35, 53));
+        timeField.setBorder(BorderFactory.createEmptyBorder());
+
+        result.setFont(new Font("Tahoma", Font.BOLD, 20));
+        result.setForeground(new Color(	111, 138, 233));
+        lSec.setForeground(new Color(	111, 138, 233));
 
 
-
-        panelTime.add(timeField);
-        panelTime.add(lSec);
-        panelTime.setBackground(Color.LIGHT_GRAY);
+        panelTime.add(timeField).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelTime.add(lSec).setFont(new Font("Tahoma", Font.BOLD, 20));
+        panelTime.setBackground(new Color(38, 35, 53));
         panelTime.setBorder(BorderFactory.createEmptyBorder(5, 0, 10, 0));
+
 
         panelResult.add(result,BorderLayout.NORTH);
         panelResult.add(panelTime,FlowLayout.CENTER);
 
 
-        panelResult.setBackground(Color.LIGHT_GRAY);
+
+        panelResult.setBackground(new Color(38, 35, 53));
+
+
     }
     public void stop(){
         timer.stop();
@@ -313,5 +379,23 @@ class LimitJTextField extends PlainDocument {
         if ((getLength() + text.length()) <= max) {
             super.insertString(offset, text, attr);
         }
+    }
+}
+
+class RoundBtn extends Rectangle implements Border
+{
+    private int r;
+    RoundBtn(int r) {
+        this.r = r;
+    }
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.r+1, this.r+1, this.r+2, this.r);
+    }
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    public void paintBorder(Component c, Graphics g, int x, int y,
+                            int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, r, r);
     }
 }
